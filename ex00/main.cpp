36bbf856e5e7fffb7febc2c37b0e1cf1127a77bb
chelmerd <chelmerd@student.42wolfsburg.de>
2022-06-18 12:35:54 +0200

    ex00: add setRawBits call

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -8,9 +8,13 @@ int main(void) {
 
 	c = b;
 
-	std::cout << a.getRawBits() <<std::endl;
-	std::cout << b.getRawBits() <<std::endl;
-	std::cout << c.getRawBits() <<std::endl;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	a.setRawBits(0b101010);
+
+	std::cout << a.getRawBits() << std::endl;
 
 	return 0;
 }
